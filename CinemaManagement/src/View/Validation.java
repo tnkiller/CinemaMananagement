638414cf4c0229b9fe8a0f2b107fc9b1d9c182a6
ThuyNguyen <nguyenthuy010605@gmail.com>
2024-11-02 17:42:14 +0700

    new

--- CinemaManagement/src/View/Validation.java
+++ CinemaManagement/src/View/Validation.java
@@ -1,3 +1,4 @@
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,11 +11,11 @@ public class Validation {
     private final static String VALID_USERNAME = "^[A-z_]\\S{5}S*$";
     private final static String VALID_PASSWORD = "^[A-z_]\\S{6}S*$";
 
-    public static String checkInputUsername(String msg){
+    public static String checkInputUsername(String msg) {
         System.out.println("Enter username:");
         while (true) {
             String result = checkInputString(msg);
-            if(result.matches(VALID_USERNAME)){
+            if (result.matches(VALID_USERNAME)) {
                 return result;
             }
             System.err.println("You must enter least at 5 character alpha, and no space");
@@ -22,156 +23,156 @@ public class Validation {
         }
     }
 
-public static String checkInputPassword(String msg){
-    System.out.println("Enter password:");
-    while (true) {
-        String result = checkInputString(msg);
-        if(result.matches(VALID_USERNAME)){
-            return result;
-        }
-        System.err.println("You must enter least at 6 character alpha, and no space");
-        System.out.print("Enter again: ");
-    }
-}
-
-public static String checkInputString(String msg) {
-    System.out.println(msg);
-    while (true) {
-        String result = input.nextLine();
-        if (!result.isEmpty()) {
-            return result;
-        }
-        System.err.println("String can't be empty");
-        System.out.print("Enter again: ");
-    }
-}
-
-public static int checkInputInt(String msg) {
-    System.out.println(msg);
-    while (true) {
-        try {
-            int result = Integer.parseInt(input.nextLine());
-            if (result > 0) {
+    public static String checkInputPassword(String msg) {
+        System.out.println("Enter password:");
+        while (true) {
+            String result = checkInputString(msg);
+            if (result.matches(VALID_USERNAME)) {
                 return result;
             }
-            System.err.println("Can't be a negative number");
-            System.out.print("Enter again: ");
-        } catch (NumberFormatException e) {
-            System.err.println("Please input integer");
+            System.err.println("You must enter least at 6 character alpha, and no space");
             System.out.print("Enter again: ");
         }
     }
-}
 
-
-public static double checkInputDouble(String msg) {
-    System.out.println(msg);
-    while (true) {
-        try {
-            double result = Double.parseDouble(input.nextLine());
-            if (result > 0) {
+    public static String checkInputString(String msg) {
+        System.out.println(msg);
+        while (true) {
+            String result = input.nextLine();
+            if (!result.isEmpty()) {
                 return result;
             }
-            System.err.println("Can't be a negative number");
-            System.out.print("Enter again: ");
-        } catch (NumberFormatException e) {
-            System.err.println("Please input a double");
+            System.err.println("String can't be empty");
             System.out.print("Enter again: ");
         }
     }
-}
 
-public static String checkInputGender(String msg) {
-    System.out.println(msg);
-    while (true) {
-        String result = input.nextLine();
-        if (result.equalsIgnoreCase("m")) {
-            return "Male";
-        }
-        if (result.equalsIgnoreCase("f")) {
-            return "Female";
-        }
-        if (result.equalsIgnoreCase("u")) {
-            return "Unknown";
-        }
-        System.err.println("Please enter m/f/u");
-        System.out.print("Enter again: ");
-    }
-}
-
-public static boolean checkInputTF(String msg) {
-    while (true) {
-        String result = checkInputString(msg);
-        if (result.equalsIgnoreCase("T")) {
-            return true;
-        }
-        if (result.equalsIgnoreCase("F")) {
-            return false;
-        }
-        System.err.println("Please enter t/T or f/F");
-        System.out.print("Enter again: ");
-    }
-}
-
-public static boolean checkInputYN(String msg) {
-    System.out.println(msg);
-    while (true) {
-        String result = input.nextLine();
-        if (result.equalsIgnoreCase("Y")) {
-            return true;
-        }
-        if (result.equalsIgnoreCase("N")) {
-            return false;
-        }
-        System.err.println("Please input y/Y or n/N.");
-        System.out.print("Enter again: ");
-    }
-}
-
-public static String checkInputCode(String msg) {
-    while (true) {
-        String result = checkInputString(msg);
-        if (result.length() == 4) {
-            return result;
-        }
-        System.err.println("Tour's code must have 4 characters");
-        System.out.print("Enter again: ");
-    }
-}
-
-public static Date checkInputDate(String msg) {
-    SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy");
-    Date date = new Date();
-    while (true) {
-        String result = checkInputString(msg);
-        try {
-            date = formater.parse(result);
-            return date;
-        } catch (ParseException e) {
-            System.err.println("Please input in format (dd/MM/yyyy)");
-            System.out.print("Enter again: ");
-        }
-    }
-}
-public static int checkInputIntLimit(int min, int max){
-    while (true){
-        try {
-            int result = Integer.parseInt(input.nextLine().trim());
-            if (result < min || result > max) {
-                throw new NumberFormatException();
+    public static int checkInputInt(String msg) {
+        System.out.println(msg);
+        while (true) {
+            try {
+                int result = Integer.parseInt(input.nextLine());
+                if (result > 0) {
+                    return result;
+                }
+                System.err.println("Can't be a negative number");
+                System.out.print("Enter again: ");
+            } catch (NumberFormatException e) {
+                System.err.println("Please input integer");
+                System.out.print("Enter again: ");
             }
-            return result;
-        }catch (NumberFormatException e) {
-            System.err.println("Please input number in rage [" + min + ", " + max + "]");
+        }
+    }
+
+    public static double checkInputDouble(String msg) {
+        System.out.println(msg);
+        while (true) {
+            try {
+                double result = Double.parseDouble(input.nextLine());
+                if (result > 0) {
+                    return result;
+                }
+                System.err.println("Can't be a negative number");
+                System.out.print("Enter again: ");
+            } catch (NumberFormatException e) {
+                System.err.println("Please input a double");
+                System.out.print("Enter again: ");
+            }
+        }
+    }
+
+    public static String checkInputGender(String msg) {
+        System.out.println(msg);
+        while (true) {
+            String result = input.nextLine();
+            if (result.equalsIgnoreCase("m")) {
+                return "Male";
+            }
+            if (result.equalsIgnoreCase("f")) {
+                return "Female";
+            }
+            if (result.equalsIgnoreCase("u")) {
+                return "Unknown";
+            }
+            System.err.println("Please enter m/f/u");
             System.out.print("Enter again: ");
         }
     }
-}
+
+    public static boolean checkInputTF(String msg) {
+        while (true) {
+            String result = checkInputString(msg);
+            if (result.equalsIgnoreCase("T")) {
+                return true;
+            }
+            if (result.equalsIgnoreCase("F")) {
+                return false;
+            }
+            System.err.println("Please enter t/T or f/F");
+            System.out.print("Enter again: ");
+        }
+    }
+
+    public static boolean checkInputYN(String msg) {
+        System.out.println(msg);
+        while (true) {
+            String result = input.nextLine();
+            if (result.equalsIgnoreCase("Y")) {
+                return true;
+            }
+            if (result.equalsIgnoreCase("N")) {
+                return false;
+            }
+            System.err.println("Please input y/Y or n/N.");
+            System.out.print("Enter again: ");
+        }
+    }
+
+    public static String checkInputCode(String msg) {
+        while (true) {
+            String result = checkInputString(msg);
+            if (result.length() == 4) {
+                return result;
+            }
+            System.err.println("Tour's code must have 4 characters");
+            System.out.print("Enter again: ");
+        }
+    }
+
+    public static Date checkInputDate(String msg) {
+        SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        while (true) {
+            String result = checkInputString(msg);
+            try {
+                date = formater.parse(result);
+                return date;
+            } catch (ParseException e) {
+                System.err.println("Please input in format (dd/MM/yyyy)");
+                System.out.print("Enter again: ");
+            }
+        }
+    }
+
+    public static int checkInputIntLimit(int min, int max) {
+        while (true) {
+            try {
+                int result = Integer.parseInt(input.nextLine().trim());
+                if (result < min || result > max) {
+                    throw new NumberFormatException();
+                }
+                return result;
+            } catch (NumberFormatException e) {
+                System.err.println("Please input number in rage [" + min + ", " + max + "]");
+                System.out.print("Enter again: ");
+            }
+        }
+    }
+
     public static String validateUsernameFormat(String username) {
         // Define regular expressions for each format
         String adminPattern = "^ADM\\d{4}$";        // ADM followed by exactly 4 digits
         String employeePattern = "^EMP\\d{6}$";     // EMP followed by exactly 6 digits
-
 
         while (true) {
             if (Pattern.matches(adminPattern, username)) {
@@ -184,14 +185,15 @@ public static int checkInputIntLimit(int min, int max){
 
         }
     }
+
     public static Date changeStringToDate(String dateString) throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         return dateFormat.parse(dateString);
     }
+
     public static String changeDateToString(Date date) throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         return dateFormat.format(date);
     }
-
 
 }
